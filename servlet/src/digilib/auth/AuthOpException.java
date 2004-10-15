e32f5205ca7f79fdf215fba076333dbf737dc3cd
Robert Casties <robcast@berlios.de>
2004-10-15 16:59:48 +0200

    Servlet version 1.22b1 - more fast searching (hopefully all working now) - some simple synchronisation - some reshuffling of methods to eliminate cruft

--- servlet/src/digilib/auth/AuthOpException.java
+++ servlet/src/digilib/auth/AuthOpException.java
@@ -1,30 +1,33 @@
 /*  AuthOpException -- Authentication exception class
 
-  Digital Image Library servlet components
+ Digital Image Library servlet components
 
-  Copyright (C) 2001, 2002 Robert Casties (robcast@mail.berlios.de)
+ Copyright (C) 2001, 2002 Robert Casties (robcast@mail.berlios.de)
 
-  This program is free software; you can redistribute  it and/or modify it
-  under  the terms of  the GNU General  Public License as published by the
-  Free Software Foundation;  either version 2 of the  License, or (at your
-  option) any later version.
-   
-  Please read license.txt for the full details. A copy of the GPL
-  may be found at http://www.gnu.org/copyleft/lgpl.html
+ This program is free software; you can redistribute  it and/or modify it
+ under  the terms of  the GNU General  Public License as published by the
+ Free Software Foundation;  either version 2 of the  License, or (at your
+ option) any later version.
+ 
+ Please read license.txt for the full details. A copy of the GPL
+ may be found at http://www.gnu.org/copyleft/lgpl.html
 
-  You should have received a copy of the GNU General Public License
-  along with this program; if not, write to the Free Software
-  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
-*/
+ */
 
 package digilib.auth;
 
 public class AuthOpException extends Exception {
 
-  public AuthOpException() {
-  }
-  public AuthOpException(String s) {
-    super(s);
-  }
+	private static final long serialVersionUID = 4908131122710826645L;
+
+	public AuthOpException() {
+	}
+
+	public AuthOpException(String s) {
+		super(s);
+	}
 }
