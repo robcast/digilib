bcd0eb37348a8e5af757603fda89cd674008b33d
Robert Casties <robcast@berlios.de>
2012-01-10 17:44:18 +0100

    upped version nr

--- webapp/src/main/webapp/jquery/jquery.digilib.js
+++ webapp/src/main/webapp/jquery/jquery.digilib.js
@@ -38,7 +38,7 @@ if (typeof console === 'undefined') {
 
     var defaults = {
         // version of this script
-        'version' : 'jquery.digilib.js 2.1b1',
+        'version' : 'jquery.digilib.js 2.1b2',
         // logo url
         'logoUrl' : 'img/digilib-logo-text1.png',
         // homepage url (behind logo)
