b62829b9db141373a1381c02ad66d84c51e43134
Robert Casties <robcast@berlios.de>
2012-05-30 14:37:05 +0200

    more work on new icons.

--- webapp/src/main/webapp/jquery/img/gen_fullscreen_icons.sh
+++ webapp/src/main/webapp/jquery/img/gen_fullscreen_icons.sh
@@ -2,20 +2,22 @@
 #
 # script for creating PNG icons from icon SVG file
 
-SVGFILE="fullscreen2.svg"
-XSLTFILE="fullscreen_icons.xsl"
+SVGFILE="fullscreen3.svg"
+XSLTFILE="gen_fullscreen_icons.xsl"
 SVGDIR="fullscreen/svg_gen"
 # PNG directory relative to SVGDIR
 PNGDIR="32"
 
 # run XSLT to create separate SVG files (in fullscreen/svg)
-SAXON="java -jar /Volumes/Schlachteplatte/stuff/java/lib/saxonb9-1-0-7j/saxon9.jar"
+#SAXON="java -jar /Volumes/Schlachteplatte/stuff/java/lib/saxonb9-1-0-7j/saxon9.jar"
+SAXON="java -jar /Users/casties/tmp/java/SaxonHE9-4-0-3J/saxon9he.jar"
 echo "running Saxon to create separate SVGs"
 mkdir -p $SVGDIR
 $SAXON $SVGFILE $XSLTFILE
 
 # create PNGs from separate SVGs using inkscape
-INKSCAPE=" /Volumes/Schlachteplatte/Applications/Inkscape.app/Contents/Resources/bin/inkscape"
+#INKSCAPE=" /Volumes/Schlachteplatte/Applications/Inkscape.app/Contents/Resources/bin/inkscape"
+INKSCAPE=" /Applications/Inkscape.app/Contents/Resources/bin/inkscape"
 echo "running Inkscape to create PNGs"
 cd $SVGDIR
 mkdir -p $PNGDIR
