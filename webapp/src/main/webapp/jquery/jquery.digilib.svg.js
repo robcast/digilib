e7d97dffe1faca08bafed9dacf9741888a44fbcc
Martin Raspe <hertzhaft@berlios.de>
2012-11-25 21:01:13 +0100

    steps towards a svg measuring tool

--- webapp/src/main/webapp/jquery/jquery.digilib.svg.js
+++ webapp/src/main/webapp/jquery/jquery.digilib.svg.js
@@ -26,42 +26,106 @@ Authors:
 
 (function($) {
 
-    // plugin object with digilib data
+    // the digilib object
     var digilib = null;
+    // the normal zoom area
+    var FULL_AREA = null;
     // the functions made available by digilib
-    var fn = null;
+    var fn = {
+        // dummy function to avoid errors, gets overwritten by buttons plugin
+        highlightButtons : function () {
+            console.debug('svg: dummy function - highlightButtons');
+            }
+        };
     // affine geometry plugin
     var geom = null;
+    // convenience variable, set in init()
+    var CSS = '';
 
-    var defaults = {};
+    var buttons = {
+        toolbar : {
+            onclick : "toolbar",
+            tooltip : "show toolbar",
+            icon : "showregions.png"
+            },
+        line : {
+            onclick : "line",
+            tooltip : "draw measuring line",
+            icon : "addregion.png"
+            }
+        };
+
+    var defaults = {
+        // buttonset of this plugin
+        svgSet : ['toolbar', 'line'],
+        // choice of colors offered by toolbar
+        lineColors : ['white', 'red', 'yellow', 'green', 'blue', 'black'],
+        // default color
+        lineColor : 'white',
+        // color while the line is drawn
+        drawColor : 'green',
+        // color of selected objects
+        selectColor : 'red',
+        // drawing shapes
+        shapes : ['line', 'polyline', 'rectangle', 'square', 'circle', 'arch',
+            'ratio', 'intercolumnium', 'line grid'],
+        // default shape
+        shape : 'line',
+        // measuring unit (index into list)
+        unit : 1,
+        // converted unit (index into list)
+        converted : 2,
+        // last measured distance 
+        lastDistance : 0,
+        // last measured angle 
+        lastAngle : 0,
+        // maximal denominator for mixed fractions
+        maxDenominator : 20,
+        // number of decimal places for convert results
+        maxDecimals : 3,
+        // show convert result as mixed fraction?
+        showMixedFraction : false,
+        // show angle relative to last line?
+        showRelativeAngle : false,
+        // show distance numbers?
+        showDistanceNumbers : true,
+        // show ratio of rectangle sides?
+        showRectangleRatios : false,
+        // draw line ends as small crosses
+        drawEndPoints : true,
+        // draw mid points of lines
+        drawMidPoints : false,
+        // draw circle centers
+        drawCenters : false,
+        // draw rectangles from the diagonal and one point
+        drawFromDiagonal : false,
+        // draw circles from center
+        drawFromCenter : false,
+        // snap to endpoints
+        snapEndPoints : false,
+        // snap to mid points of lines
+        snapMidPoints : false,
+        // snap to circle centers
+        snapCenters : false,
+        // snap distance (in screen pixels)
+        snapDistance : 5,
+        // keep original object when moving/scaling/rotating
+        keepOriginal : false,
+        // number of copies when drawing grids
+        gridCopies : 10
+        };
 
     var actions = {
-        "test" : function(options) {
-            var onLoadXML = function (xml) {
-                settings.xml = xml;
-                settings.$toolBar = setupToolBar(settings);
-                $digilib.each(function() {
-                    var $elem = $(this);
-                    $elem.data(pluginName, settings);
-                    });
+        toolbar : function(data) {
+            var $toolbar = data.settings.$toolbar;
+            if ($toolbar) {
+                return $toolbar.toggle();
                 };
-            var onLoadScalerImg = function () {
-                var $svgDiv = $('<div id="svg" />');
-                $('body').append($svgDiv);
-                // size SVG div like scaler img
-                var $scalerImg = $digilib.find('img.pic');
-                var scalerImgRect = geom.rectangle($scalerImg);
-                scalerImgRect.adjustDiv($svgDiv);
-                console.debug('$svgDiv', scalerImgRect);
-                var $svg = $svgDiv.svg({
-                        'onLoad' : drawInitial
-                    });
-                settings.$elem = $digilib;
-                settings.$svgDiv = $svgDiv;
-                settings.$svg = $svg;
-                // set SVG data 
-                $svg.data('digilib', data);
-                $svg.data(pluginName, settings);
+            $toolbar = setupToolbar(data);
+            var onLoadXML = function (xml) {
+                $xml = $(xml);
+                data.settings.$xml = $xml;
+                loadToolbarData(data, $toolbar, $xml);
                 };
             // fetch the XML measuring unit list
             $.ajax({
@@ -70,10 +134,107 @@ Authors:
                 dataType : "xml",
                 success : onLoadXML
                 });
-            data.$img.load(onLoadScalerImg);
+            // data.$img.load(onLoadScalerImg);
             return this;
+            },
+        line : function(data) {
+            //
             }
         };
+
+    // setup a div for accessing the main SVG functionality
+    var setupToolbar = function(data) {
+        var html = '\
+            <div id="svg-toolbar">\
+                <select id="svg-shapes"/>\
+                <span class="svg-label">pixel</span>\
+                <span id="svg-pixel" class="svg-number">0.0</span>\
+                <span class="svg-label">factor</span>\
+                <span id="svg-factor" class="svg-number">0.0</span>\
+                <input id="svg-value1" class="svg-input" value="0.0"/>\
+                <span class="svg-label">=</span>\
+                <select id="svg-unit1"/>\
+                <input id="svg-value2" class="svg-input" value="0.0"/>\
+                <span class="svg-label">=</span>\
+                <select id="svg-unit2"/>\
+                <span id="svg-angle" class="svg-number">0.0</span>\
+            </div>';
+        var $toolbar = $(html);
+        data.$elem.append($toolbar);
+        data.settings.$toolbar = $toolbar;
+        $toolbar.show();
+        return $toolbar;
+        };
+
+    // load shapes into select element
+    var loadShapes = function(data, $toolbar) {
+        var $shape = $toolbar.find('#svg-shapes');
+        $.each(data.settings.shapes, function(index, name) {
+            var $opt = $('<option value="'+index+'">'+name+'</option>');
+            $shape.append($opt);
+            });
+    };
+
+    // load units into select elements
+    var loadSections = function(data, $toolbar, $xml) {
+        var $unit1 = $toolbar.find('#svg-unit1');
+        var $unit2 = $toolbar.find('#svg-unit2');
+        $xml.find("section").each(function(index, section) {
+            var $section = $(section);
+            var name = $section.attr("name");
+            var $opt = $('<option class="section" disabled="disabled">'+name+'</option>');
+            $unit1.append($opt);
+            $unit2.append($opt.clone());
+            $section.find("unit").each(function(index, unit) {
+                var $unit = $(unit);
+                var name = $unit.attr("name");
+                var factor = $unit.attr("factor"); 
+                var $opt = $('<option class="unit" value="'+factor+'">'+ name+'</option>');
+                $opt.data('unit', {
+                    'name' : name,
+                    'factor' : factor, 
+                    'add' : $unit.attr("add"), 
+                    'subunits' : $unit.attr("subunits")
+                    });
+                $unit1.append($opt);
+                $unit2.append($opt.clone());
+                });
+            });
+    };
+
+    // setup a div for accessing the main SVG functionality
+    var loadToolbarData = function(data, $toolbar, $xml) {
+        loadShapes(data, $toolbar);
+        loadSections(data, $toolbar, $xml);
+    };
+
+    var drawInitial = function ($svg) {
+        console.debug('SVG is ready');
+        var $svgDiv = $(this);
+        var rect = geom.rectangle($svgDiv);
+        $svg.line(0, 0, rect.width, rect.height,
+            {stroke: 'white', strokeWidth: 2});
+        $svg.line(0, rect.height, rect.width, 0,
+            {stroke: 'red', strokeWidth: 2});
+        };
+
+    var handleSetup = function (evt) {
+        console.debug("svg: handleSetup");
+        var data = this;
+        var settings = data.settings;
+    };
+
+    // additional buttons
+    var installButtons = function (data) {
+        var settings = data.settings;
+        var mode = settings.interactionMode;
+        var buttonSettings = settings.buttonSettings[mode];
+        var buttonSet = settings.svgSet;
+        if (buttonSet.length && buttonSet.length > 0) {
+            buttonSettings.svgSet = buttonSet;
+            buttonSettings.buttonSets.push('svgSet');
+            }
+    };
 
     // plugin installation called by digilib on plugin object.
     var install = function (plugin) {
@@ -83,28 +244,32 @@ Authors:
         // import geometry classes
         geom = fn.geometry;
         // add defaults, actions, buttons
-        $.extend(true, digilib.defaults, defaults); // make deep copy
+        $.extend(true, digilib.defaults, defaults);
         $.extend(digilib.actions, actions);
+        $.extend(true, digilib.buttons, buttons);
         // export functions
         // fn.test = test;
     };
 
     // plugin initialization
     var init = function (data) {
-        console.debug('initialising digilibSVG plugin. data:', data);
-        var $data = $(data);
-        $data.bind('setup', handleSetup);
-    };
-
-    var handleSetup = function (evt) {
-        console.debug("svg: handleSetup");
-        var data = this;
+        console.debug('initialising svg plugin. data:', data);
         var settings = data.settings;
+        CSS = settings.cssPrefix;
+        FULL_AREA  = geom.rectangle(0, 0, 1, 1);
+        // install event handlers
+        var $data = $(data);
+        $data.on('setup', handleSetup);
+        // $data.on('update', handleUpdate);
+        // install region buttons if user defined regions are allowed
+        if (digilib.plugins.buttons != null) {
+            installButtons(data);
+        }
     };
 
     // plugin object with name and init
     // shared objects filled by digilib on registration
-    var plugin = {
+    var pluginProperties = {
             name : 'svg',
             install : install,
             init : init,
@@ -117,6 +282,6 @@ Authors:
     if ($.fn.digilib == null) {
         $.error("jquery.digilib.svg must be loaded after jquery.digilib!");
     } else {
-        $.fn.digilib('plugin', plugin);
+        $.fn.digilib('plugin', pluginProperties);
     }
 })(jQuery);
