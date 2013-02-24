0e828a9cc27dfb7a7ae98765ab5ce5d35218f8ba
Robert Casties <robcast@berlios.de>
2013-02-24 11:18:58 +0100

    starting to clean license headers.

--- servlet3/src/main/java/digilib/servlet/ServletOps.java
+++ servlet3/src/main/java/digilib/servlet/ServletOps.java
@@ -1,25 +1,30 @@
+package digilib.servlet;
+
 /*
+ * #%L
+ * 
  * ServletOps -- Servlet utility class
  * 
  * Digital Image Library servlet components
  * 
- * Copyright (C) 2001, 2002 Robert Casties (robcast@mail.berlios.de)
+ * %%
+ * Copyright (C) 2001 - 2013 Robert Casties (robcast@mail.berlios.de)
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as 
+ * published by the Free Software Foundation, either version 3 of the 
+ * License, or (at your option) any later version.
  * 
- * This program is free software; you can redistribute it and/or modify it under
- * the terms of the GNU General Public License as published by the Free Software
- * Foundation; either version 2 of the License, or (at your option) any later
- * version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Lesser Public License for more details.
  * 
- * Please read license.txt for the full details. A copy of the GPL may be found
- * at http://www.gnu.org/copyleft/lgpl.html
- * 
- * You should have received a copy of the GNU General Public License along with
- * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
- * Place, Suite 330, Boston, MA 02111-1307 USA
- *  
+ * You should have received a copy of the GNU General Lesser Public 
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/lgpl-3.0.html>.
+ * #L%
  */
-
-package digilib.servlet;
 
 import java.io.File;
 import java.io.FileInputStream;
