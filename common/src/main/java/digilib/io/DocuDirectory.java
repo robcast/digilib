ba35b5f65c382b7022318fa5c822f4d20dadfc5c
Robert Casties <robcast@berlios.de>
2013-02-25 11:36:31 +0100

    more cleaner license headers.

--- common/src/main/java/digilib/io/DocuDirectory.java
+++ common/src/main/java/digilib/io/DocuDirectory.java
@@ -1,25 +1,30 @@
-/* DocuDirectory -- Directory of DocuFilesets.
+package digilib.io;
 
- Digital Image Library servlet components
-
- Copyright (C) 2003 Robert Casties (robcast@mail.berlios.de)
-
- This program is free software; you can redistribute  it and/or modify it
- under  the terms of  the GNU General  Public License as published by the
- Free Software Foundation;  either version 2 of the  License, or (at your
- option) any later version.
- 
- Please read license.txt for the full details. A copy of the GPL
- may be found at http://www.gnu.org/copyleft/lgpl.html
-
- You should have received a copy of the GNU General Public License
- along with this program; if not, write to the Free Software
- Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
-
+/*
+ * #%L
+ * DocuDirectory -- Directory of DocuFilesets.
+ * 
+ * Digital Image Library servlet components
+ * 
+ * %%
+ * Copyright (C) 2003 - 2013 Robert Casties (robcast@mail.berlios.de)
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as 
+ * published by the Free Software Foundation, either version 3 of the 
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Lesser Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Lesser Public 
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/lgpl-3.0.html>.
+ * #L%
  * Created on 25.02.2003
  */
-
-package digilib.io;
 
 import java.io.File;
 import java.io.IOException;
--- common/src/main/java/digilib/io/ImageSet.java
+++ common/src/main/java/digilib/io/ImageSet.java
@@ -1,21 +1,29 @@
-/* ImageSet -- digilib image file info class.  
- * Digital Image Library servlet components  
- * Copyright (C) 2003 Robert Casties (robcast@mail.berlios.de)  
- * 
- * This program is free software; you can
- * redistribute it and/or modify it under the terms of the GNU General Public
- * License as published by the Free Software Foundation; either version 2 of
- * the License, or (at your option) any later version.  
- * 
- * Please read license.txt for the full details. A copy of the GPL may be 
- * found at http://www.gnu.org/copyleft/lgpl.html  
- * 
- * You should have received a copy of the GNU General Public License along 
- * with this program; if not, write to the Free Software Foundation, Inc., 
- * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA  
- */
-
 package digilib.io;
+
+/*
+ * #%L
+ * ImageSet -- digilib class for image file information at different resolutions.
+ *   
+ * Digital Image Library servlet components
+ *   
+ * %%
+ * Copyright (C) 2003 - 2013 Robert Casties (robcast@mail.berlios.de)
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as 
+ * published by the Free Software Foundation, either version 3 of the 
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Lesser Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Lesser Public 
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/lgpl-3.0.html>.
+ * #L%
+ */
 
 import java.util.ArrayList;
 import java.util.List;
