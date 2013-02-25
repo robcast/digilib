d6a85a196e2283b6cac213b1435a7ee933ad6080
Robert Casties <robcast@berlios.de>
2013-02-25 15:20:44 +0100

    more clean license headers.

--- common-jai/src/main/java/digilib/image/JAIDocuImage.java
+++ common-jai/src/main/java/digilib/image/JAIDocuImage.java
@@ -1,24 +1,26 @@
-/* JAIDocuImage -- Image class implementation using JAI (Java Advanced Imaging)
-
- Digital Image Library servlet components
-
- Copyright (C) 2001, 2002, 2003 Robert Casties (robcast@mail.berlios.de)
-
- This program is free software; you can redistribute  it and/or modify it
- under  the terms of  the GNU General  Public License as published by the
- Free Software Foundation;  either version 2 of the  License, or (at your
- option) any later version.
- 
- Please read license.txt for the full details. A copy of the GPL
- may be found at http://www.gnu.org/copyleft/lgpl.html
-
- You should have received a copy of the GNU General Public License
- along with this program; if not, write to the Free Software
- Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-
- */
-
 package digilib.image;
+
+/*
+ * #%L
+ * JAIDocuImage -- Image class implementation using JAI (Java Advanced Imaging)
+ * %%
+ * Copyright (C) 2001 - 2013 Robert Casties (robcast@mail.berlios.de)
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as 
+ * published by the Free Software Foundation, either version 3 of the 
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Lesser Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Lesser Public 
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/lgpl-3.0.html>.
+ * #L%
+ */
 
 import java.awt.Rectangle;
 import java.awt.RenderingHints;
@@ -59,9 +61,12 @@ public class JAIDocuImage extends ImageInfoDocuImage {
 	protected Interpolation interpol = null;
 
 	/*
-	 * static { // we could set our own tile cache size here TileCache tc =
+	 * static { 
+	 * // we could set our own tile cache size here 
+	 * TileCache tc =
 	 * JAI.createTileCache(100*1024*1024);
-	 * JAI.getDefaultInstance().setTileCache(tc); }
+	 * JAI.getDefaultInstance().setTileCache(tc); 
+	 * }
 	 */
 
 	public boolean isSubimageSupported() {
