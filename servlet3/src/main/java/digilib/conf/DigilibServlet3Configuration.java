0e784ef97f79f77b8bb4c299ff28d1d93f5c9931
Robert Casties <robcast@berlios.de>
2013-04-17 12:56:42 +0200

    add some comments.

--- servlet3/src/main/java/digilib/conf/DigilibServlet3Configuration.java
+++ servlet3/src/main/java/digilib/conf/DigilibServlet3Configuration.java
@@ -1,11 +1,40 @@
 package digilib.conf;
 
+/*
+ * #%L
+ * DigilibServlet3Configuration -- Holding all parameters for digilib servlet.
+ * 
+ * Digital Image Library servlet components
+ * 
+ * %%
+ * Copyright (C) 2013 MPIWG Berlin
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as 
+ * published by the Free Software Foundation, either version 3 of the 
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Lesser Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Lesser Public 
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/lgpl-3.0.html>.
+ * #L%
+ * Author: Robert Casties (robcast@berlios.de)
+ */
+
 import javax.servlet.ServletContext;
 import javax.servlet.annotation.WebListener;
 
 import digilib.servlet.AsyncServletWorker;
 
 /**
+ * Class to hold the digilib servlet configuration parameters. The parameters
+ * can be read from the digilib-config file and be passed to other servlets or
+ * beans.
  * @author casties
  *
  */
