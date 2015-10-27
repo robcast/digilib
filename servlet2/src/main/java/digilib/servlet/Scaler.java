38c05278e0c7a06a5412957c85a933bcf27736e2
Robert Casties <robcast@users.sourceforge.net>
2015-10-27 11:21:32 +0100

    more inheritance of version numbers.
    --HG--
    branch : new_scaling

--- servlet2/src/main/java/digilib/servlet/Scaler.java
+++ servlet2/src/main/java/digilib/servlet/Scaler.java
@@ -61,7 +61,7 @@ public class Scaler extends HttpServlet {
     private static final long serialVersionUID = -5439198888139362735L;
 
     /** digilib servlet version (for all components) */
-    public static final String version = "2.3.1 noasync";
+    public static final String version = DigilibServletConfiguration.getClassVersion() + " noasync";
 
     /** servlet error codes */
     public static enum Error {
