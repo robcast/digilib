ba9c84f682c1397f49a9072b3a8b531b3ec5ee2b
Robert Casties <casties@mpiwg-berlin.mpg.de>
2018-03-26 19:28:08 +0200

    Update version numbers to 2.5.6.

--- common/src/main/java/digilib/conf/DigilibConfiguration.java
+++ common/src/main/java/digilib/conf/DigilibConfiguration.java
@@ -57,7 +57,7 @@ public class DigilibConfiguration extends ParameterMap {
 
     /** digilib version */
     public static String getClassVersion() {
-        return "2.5.6a";
+        return "2.5.6";
     }
 
     /* non-static getVersion for Java inheritance */
