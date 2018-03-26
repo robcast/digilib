f901264b3efbcddde6bda5af693ab43ccc23eaac
Robert Casties <casties@mpiwg-berlin.mpg.de>
2018-03-26 19:49:46 +0200

    starting digilib 2.6.

--- common/src/main/java/digilib/conf/DigilibConfiguration.java
+++ common/src/main/java/digilib/conf/DigilibConfiguration.java
@@ -57,7 +57,7 @@ public class DigilibConfiguration extends ParameterMap {
 
     /** digilib version */
     public static String getClassVersion() {
-        return "2.5.6a";
+        return "2.6.0a";
     }
 
     /* non-static getVersion for Java inheritance */
