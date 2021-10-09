93bf268d2ef503691469884abc4b58e7fe3eb997
Robert Casties <casties@mpiwg-berlin.mpg.de>
2021-10-09 17:56:15 +0200

    small cleanup.

--- pdf/src/main/java/digilib/pdf/PDFTitlePage.java
+++ pdf/src/main/java/digilib/pdf/PDFTitlePage.java
@@ -1,29 +1,5 @@
 package digilib.pdf;
 
-/*
- * #%L
- * A class for the generation of title pages for the generated pdf documents.
- * %%
- * Copyright (C) 2009 MPIWG Berlin
- * %%
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as 
- * published by the Free Software Foundation, either version 3 of the 
- * License, or (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Lesser Public License for more details.
- * 
- * You should have received a copy of the GNU General Lesser Public 
- * License along with this program.  If not, see
- * <http://www.gnu.org/licenses/lgpl-3.0.html>.
- * #L%
- * Authors: Christopher Mielack, Robert Casties
- */
-
-import java.io.File;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -45,9 +21,6 @@ import com.itextpdf.layout.property.TextAlignment;
 import com.itextpdf.layout.property.UnitValue;
 
 import digilib.conf.PDFRequest;
-import digilib.image.ImageOpException;
-import digilib.io.FileOpException;
-import digilib.io.FsDocuDirectory;
 
 /**
  * A class for the generation of a title page for the generated pdf documents.
