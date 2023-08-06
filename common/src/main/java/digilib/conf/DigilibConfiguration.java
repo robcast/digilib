27849f253c201bcf41455d11e226667a09f3fc45
Robert Casties <casties@mpiwg-berlin.mpg.de>
2023-08-06 17:47:43 +0200

    new version 2.12.3

--- common/src/main/java/digilib/conf/DigilibConfiguration.java
+++ common/src/main/java/digilib/conf/DigilibConfiguration.java
@@ -64,7 +64,7 @@ public class DigilibConfiguration extends ParameterMap {
      * @return the version
      */
     public static String getClassVersion() {
-        return "2.12.2";
+        return "2.12.3";
     }
 
     /* non-static getVersion for Java inheritance */
