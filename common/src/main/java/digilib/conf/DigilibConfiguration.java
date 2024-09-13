20850305b9b3d4777be24911768d9f6b8ca2185d
Robert Casties <casties@mpiwg-berlin.mpg.de>
2024-09-13 11:31:40 +0200

    update missing version number.

--- common/src/main/java/digilib/conf/DigilibConfiguration.java
+++ common/src/main/java/digilib/conf/DigilibConfiguration.java
@@ -64,7 +64,7 @@ public class DigilibConfiguration extends ParameterMap {
      * @return the version
      */
     public static String getClassVersion() {
-        return "2.12.3";
+        return "2.12.4";
     }
 
     /* non-static getVersion for Java inheritance */
